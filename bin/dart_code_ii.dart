eedc61c7e1a515d481fea5be2e5c4d1290e2fc92
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-04-20 01:21:18 +0600

    Practices

---
+++ bin/dart_code_ii.dart
@@ -0,0 +1,5 @@
+import 'package:dart_code_ii/dart_code_ii.dart' as dart_code_ii;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_code_ii.calculate()}!');
+}
---
+++ bin/problem_solving.dart
@@ -0,0 +1,30 @@
+///Write a program that prints the even numbers from 1 to 10.
+
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+///Write a program that asks the user for a number and then prints
+///the times table for that number (i.e., 1 times the number, 2 times the number, etc. up to 10 times the number).
+
+
+
+///Write a program that takes a list of numbers and prints the sum of all the even numbers in the list.
+
+///Write a program that takes a list of names and returns a new list with only the names that start with the letter "S".
+
+///Write a program that takes a string and prints the frequency of each character in the string.
+
+///Write a program that takes two sets of numbers and returns a set containing only the numbers that appear in both sets.
+
+///Write a program that takes a list of numbers and returns a new list containing only the unique numbers (i.e., removing duplicates).
+
+///Write a program that takes a list of strings and returns a new list containing only the strings that are longer than 5 characters.
+
+///Write a program that takes a map of student names and their grades and returns a new map with the names of the students who scored above 90%.
+
+///Write a program that takes a list of numbers and returns the largest and smallest numbers in the list.
