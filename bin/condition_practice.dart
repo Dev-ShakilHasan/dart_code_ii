671cb1d91d774f3d1f446e89b6007ce5c4112fae
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-04-23 23:28:11 +0600

    Practice

---
+++ bin/condition_practice.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+///Write a Dart program that asks the user to enter their age and then prints out
+/// whether they are eligible to vote or not.
+/// The program should use an if-else statement to determine the user's eligibility based on the age entered.
+/// In most countries, the legal voting age is 18.
+
+void main() {
+  int ageOfTheUser;
+  ageOfTheUser=25;
+
+  if (ageOfTheUser >= 18 && ageOfTheUser <= 100)
+  {
+    print('You are eligible for voting');
+  }
+  else
+  {
+    print('You are not eligible');
+  }
+}
---
+++ bin/function.dart
@@ -0,0 +1,24 @@
+///Create a function that takes two parameters, a number and a list of numbers.
+/// The function should return true if the number appears in the list and false otherwise.
+
+// void main() {
+//   List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   print(containSameNumber(3, numberList));
+// }
+//
+// bool containSameNumber(int randomNumber, List<int> numberList) {
+//   return numberList.contains(randomNumber);
+// }
+
+/// Write a function that takes a string as input and returns the string with all vowels removed.
+
+// void main(){
+//  print(removedVowelString("This is Md. Shakil Hasan"));
+// }
+//
+// String removedVowelString(String givenString){
+//   return givenString.replaceAll(RegExp('[aeiouAEIOU]'), '');
+// }
+
+///Create a function that takes an integer as input and returns a list of all
+///the prime numbers less than or equal to the input.
