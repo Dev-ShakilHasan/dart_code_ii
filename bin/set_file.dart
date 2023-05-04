2145fbfd8972ae9d19b5c8bba21a6b41f48a0c25
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-05-05 01:08:13 +0600

    Practice

---
+++ bin/set_file.dart
@@ -0,0 +1,23 @@
+void main(){
+
+Set myNameSet={'Md','Shakil', 'Hasan'};
+myNameSet.add('Flutter');
+myNameSet.addAll(['Developer','Software']);
+int setLength=myNameSet.length;
+var indexNumber=myNameSet.elementAt(3);
+
+
+for(var i in myNameSet){
+  print(i);
+}
+
+print(indexNumber);
+print(myNameSet);
+print(setLength);
+
+}
+
+
+
+
+
