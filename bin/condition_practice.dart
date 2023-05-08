595dfca34f39b3cf181af2a87a1525f878e65dbb
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-05-08 22:38:35 +0600

    Practice

--- bin/condition_practice.dart
+++ bin/condition_practice.dart
@@ -5,16 +5,22 @@
 /// The program should use an if-else statement to determine the user's eligibility based on the age entered.
 /// In most countries, the legal voting age is 18.
 
-void main() {
-  int ageOfTheUser;
-  ageOfTheUser=25;
-
-  if (ageOfTheUser >= 18 && ageOfTheUser <= 100)
-  {
-    print('You are eligible for voting');
-  }
-  else
-  {
-    print('You are not eligible');
-  }
+// void main() {
+//   int ageOfTheUser;
+//   ageOfTheUser=25;
+//
+//   if (ageOfTheUser >= 18 && ageOfTheUser <= 100)
+//   {
+//     print('You are eligible for voting');
+//   }
+//   else
+//   {
+//     print('You are not eligible');
+//   }
+// }
+///Use of Switch Case
+void main(){
+  
 }
+
+
